df641396afb4434035837afe4000e2c019d7a97e
nashwaan <ysf953@gmail.com>
2016-05-01 12:34:10 +0400

    Used istanbul code coverage tool.

---
+++ test/index.js
@@ -0,0 +1,17 @@
+/*jslint node:true */
+
+var Jasmine = require('jasmine');
+var jasmine = new Jasmine();
+
+jasmine.loadConfigFile('jasmine.json');
+
+jasmine.onComplete(function(passed) {
+    if(passed) {
+        console.log('All specs have passed');
+    }
+    else {
+        console.log('At least one spec has failed');
+    }
+});
+
+jasmine.execute();
