2fa0f6c482f6ee156eb53cf8cb4dea6f50166a13
Sam Jakos <sjakos@users.noreply.github.com>
2018-09-11 23:01:03 -0500

    Add types for custom processing functions
    Fixes #81

--- types/index.d.ts
+++ types/index.d.ts
@@ -53,6 +53,32 @@ declare namespace Options {
     alwaysChildren?: boolean
     instructionHasAttributes?: boolean
     captureSpacesBetweenElements?: boolean
+    doctypeFn?: (value: string, parentElement: string) => void;
+    instructionFn?: (
+      instructionValue: string,
+      instructionName: string,
+      parentElement: string
+    ) => void;
+    cdataFn?: (value: string, parentElement: string) => void;
+    commentFn?: (value: string, parentElement: string) => void;
+    textFn?: (value: string, parentElement: string) => void;
+    instructionNameFn?: (
+      instructionName: string,
+      instructionValue: string,
+      parentElement: string
+    ) => void;
+    elementNameFn?: (value: string, parentElement: string) => void;
+    attributeNameFn?: (
+      attributeName: string,
+      attributeValue: string,
+      parentElement: string
+    ) => void;
+    attributeValueFn?: (
+      attributeValue: string,
+      attributeName: string,
+      parentElement: string
+    ) => void;
+    attributesFn?: (value: string, parentElement: string) => void;
   }
 
   interface JS2XML extends ChangingKeyNames, IgnoreOptions {
@@ -64,6 +90,37 @@ declare namespace Options {
     indentInstruction?: boolean
     fullTagEmptyElement?: boolean
     noQuotesForNativeAttributes?: boolean
+    doctypeFn?: (value: string, currentElementName: string, currentElementObj: object) => void;
+    instructionFn?: (
+      instructionValue: string,
+      instructionName: string,
+      currentElementName: string,
+      currentElementObj: object
+    ) => void;
+    cdataFn?: (value: string, currentElementName: string, currentElementObj: object) => void;
+    commentFn?: (value: string, currentElementName: string, currentElementObj: object) => void;
+    textFn?: (value: string, currentElementName: string, currentElementObj: object) => void;
+    instructionNameFn?: (
+      instructionName: string,
+      instructionValue: string,
+      currentElementName: string,
+      currentElementObj: object
+    ) => void;
+    elementNameFn?: (value: string, currentElementName: string, currentElementObj: object) => void;
+    attributeNameFn?: (
+      attributeName: string,
+      attributeValue: string,
+      currentElementName: string,
+      currentElementObj: object
+    ) => void;
+    attributeValueFn?: (
+      attributeValue: string,
+      attributeName: string,
+      currentElementName: string,
+      currentElementObj: object
+    ) => void;
+    attributesFn?: (value: string, currentElementName: string, currentElementObj: object) => void;
+    fullTagEmptyElementFn?: (currentElementName: string, currentElementObj: object) => void;
   }
 
   interface IgnoreOptions {
